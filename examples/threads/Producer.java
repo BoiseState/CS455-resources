e19793283b6f84c9c976edd73071abf1f2be46b9
amit <ajain@boisestate.edu>
2016-01-20 20:05:07 -0700

    Update threads examples

--- examples/threads/Producer.java
+++ examples/threads/Producer.java
@@ -1,44 +1,51 @@
-import java.util.Vector;
 
-public class Producer implements Runnable
+public class Producer implements Runnable 
 {
-    private Thread producerThread;
-    private SharedQueue Q;
-    private int id;
-    private int sleep_time;
+	private Thread producerThread;
+	private SharedQueue Q;
+	private int id;
+	private int sleep_time;
 	private boolean stopFlag = false;
 
-    public Producer(int id, SharedQueue Q, int time){
-      this.Q = Q;
-      this.id = id;
-      this.sleep_time = time;
+	public Producer(int id, SharedQueue Q, int time)
+	{
+		this.Q = Q;
+		this.id = id;
+		this.sleep_time = time;
+	}
 
-    }  
-    
-    public void run() {
+	public void run()
+	{
 		try {
-			while ( !stopFlag ) {
-				String msg = new java.util.Date().toString() ;
+			while (!stopFlag)
+			{
+				String msg = new java.util.Date().toString();
 				Q.putMessage(msg);
-				System.out.println("Producer " + id + " put message: "+msg);
+				System.out.println("Producer " + id + " put message: " + msg);
 				// try commenting out the sleep
-				producerThread.sleep( sleep_time );
+				producerThread.sleep(sleep_time);
 			}
-		} catch( InterruptedException e ) { }
-    }
+		} catch (InterruptedException e) {
+			System.err.println(e);
+		}
+	}
 
-    public void start() {
-    		if (producerThread == null) {
+	public void start()
+	{
+		if (producerThread == null)
+		{
 			producerThread = new Thread(this);
 			producerThread.start();
 		}
-    }
+	}
 
-    public void stop(){
-    		if (producerThread != null) {
+	public void stop()
+	{
+		if (producerThread != null)
+		{
 			stopFlag = true;
 			producerThread = null;
 		}
-    }
+	}
 
 }
