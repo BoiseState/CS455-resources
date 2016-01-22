c0492470132f9761de8324cee936f9450e59243e
boiseamit <ajain@boisestate.edu>
2016-01-21 17:33:05 -0700

    Update reflection examples.

--- examples/reflection/ReflectOnAnother.java
+++ examples/reflection/ReflectOnAnother.java
@@ -2,35 +2,30 @@ import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
 
-
-
 /**
- * Print all public methods and fields for a user specified class name. 
- *
+ * Print all public methods and fields for a user specified class name.
  */
-public class ReflectOnAnother {
-	public static void main(String [] args) 
-	{
-		if (args.length == 0) {
-			System.err.println("Usage: java ReflectOnAnother <class name>");
-			System.exit(1);
-		}
-
-		Class<?> type = null;
-		try {
-			type = Class.forName(args[0]);
-		} catch (ClassNotFoundException e) {
-			System.err.println(e);
-		}
-		
-		Method [] methods = type.getMethods();
-		for (Method m: methods) 
-			if (Modifier.isPublic(m.getModifiers())) 
-				System.out.println("  " + m);
-		
-		Field [] fields = type.getFields();
-		for (Field f: fields) 
-			if (Modifier.isPublic(f.getModifiers()))
-				System.out.println("  " + f);
+public class ReflectOnAnother
+{
+    public static void main(String[] args) {
+	if (args.length == 0) {
+	    System.err.println("Usage: java ReflectOnAnother <class name>");
+	    System.exit(1);
 	}
+
+	Class<?> type = null;
+	try {
+	    type = Class.forName(args[0]);
+	} catch (ClassNotFoundException e) {
+	    System.err.println(e);
+	}
+
+	Method[] methods = type.getMethods();
+	for (Method m : methods)
+	    if (Modifier.isPublic(m.getModifiers())) System.out.println("  " + m);
+
+	Field[] fields = type.getFields();
+	for (Field f : fields)
+	    if (Modifier.isPublic(f.getModifiers())) System.out.println("  " + f);
+    }
 }
--- examples/reflection/TraceAncestry.java
+++ examples/reflection/TraceAncestry.java
@@ -1,38 +1,35 @@
 
 /**
  * Trace the ancestry of a specified class name.
+ * 
  * @author amit
  *
  */
 public class TraceAncestry
 {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args)
-	{
-		if (args.length == 0) {
-			System.err.println("Usage: java TraceAncestry <class name>");
-			System.exit(1);
-		}
-		
-		Class<?> type = null;
-		try {
-			type = Class.forName(args[0]);
-		} catch (ClassNotFoundException e) {
-			System.err.println(e);
-		}
-		
-		System.out.println("class " + type.getSimpleName());
-		Class<?> superclass = type;
-		do {
-			superclass = superclass.getSuperclass();
-			if (superclass == null) break;
-			System.out.println("extends " + superclass.getCanonicalName());
-		} while (!superclass.getCanonicalName().equals("java.lang.Object"));
-	
-
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+	if (args.length == 0) {
+	    System.err.println("Usage: java TraceAncestry <class name>");
+	    System.exit(1);
 	}
 
+	Class<?> type = null;
+	try {
+	    type = Class.forName(args[0]);
+	} catch (ClassNotFoundException e) {
+	    System.err.println(e);
+	}
+
+	System.out.println("class " + type.getSimpleName());
+	Class<?> superclass = type;
+	do {
+	    superclass = superclass.getSuperclass();
+	    if (superclass == null) break;
+	    System.out.println("extends " + superclass.getCanonicalName());
+	} while (!superclass.getCanonicalName().equals("java.lang.Object"));
+    }
 }
