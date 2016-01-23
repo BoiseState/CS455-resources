a21b75a7bc133c469189d12f992a5c2474a6a6d7
amit <ajain@boisestate.edu>
2016-01-23 11:38:03 -0700

    Update serialization examples

--- examples/serialization/LoadTable.java
+++ examples/serialization/LoadTable.java
@@ -1,28 +1,34 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.util.Enumeration;
+import java.util.Hashtable;
 
-/*package classExamples.io;*/
-import java.io.*;
-import java.util.*;
+public class LoadTable
+{
+    public static void main(String[] args)
+    {
+	try {
+	    FileInputStream fileIn = new FileInputStream("hash.serial");
+	    ObjectInputStream in = new ObjectInputStream(fileIn);
+	    Object obj = in.readObject();
+	    in.close();
 
-public class LoadTable {
-	public static void main (String [] args) {
-		try {
-			FileInputStream fileIn = new FileInputStream("hash.serial");
-			ObjectInputStream in = new ObjectInputStream(fileIn);
-			Object obj = in.readObject();
-			Hashtable h = (Hashtable) obj;
-			System.out.println(h.toString());
+	    Hashtable<String, Object> h = (Hashtable<String, Object>) obj;
+	    System.out.println(h.toString());
 
-			Enumeration list = h.elements();
-			while (list.hasMoreElements())
-			{
-				System.out.println(list.nextElement());
-			}
-		}
-		catch (Exception e) {
-			System.out.println(e);
-		}
+	    Enumeration<Object> list = h.elements();
+	    while (list.hasMoreElements()) {
+		System.out.println(list.nextElement());
+	    }
+
+	} catch (FileNotFoundException e) {
+	    System.err.println(e);
+	} catch (IOException e) {
+	    System.err.println(e);
+	} catch (ClassNotFoundException e) {
+	    System.err.println(e);
 	}
-}	
-
-
-
+    }
+}
--- examples/serialization/SaveTable.java
+++ examples/serialization/SaveTable.java
@@ -1,29 +1,27 @@
+import java.awt.Color;
+import java.io.FileOutputStream;
+import java.io.ObjectOutputStream;
+import java.util.Hashtable;
 
-/*package classExamples.io;*/
-import java.io.*;
-import java.util.*;
-import java.awt.*;
+public class SaveTable
+{
+    public static void main(String[] args)
+    {
+	Hashtable<String, Object> h = new Hashtable<String, Object>();
+	// Hashtable implements the serializable interface
+	// so we can freeze dry it
+	h.put("string", "Cyrano De Bergerac");
+	h.put("int", new Integer("1600"));
+	h.put("double", new Double(Math.PI));
+	h.put("color", new Color(255, 0, 0));
 
-public class SaveTable {
-	public static void main (String [] args) {
-		Hashtable<String, Object> h = new Hashtable<String, Object>(); 
-		// Hashtable implements the serializable interface
-		// so we can try to  freeze dry it
-		h.put("string","Cyrano De Bergerac");
-		h.put("int",new Integer("1600"));
-		h.put("double", new Double(Math.PI));
-		h.put("color", new Color(255,0,0));
-
-		try {
-			FileOutputStream fileout = new FileOutputStream("hash.serial");
-			ObjectOutputStream out = new ObjectOutputStream(fileout);
-			out.writeObject(h);
-		}
-		catch (Exception e) {
-			System.out.println(e);
-		}
+	try {
+	    FileOutputStream fileout = new FileOutputStream("hash.serial");
+	    ObjectOutputStream out = new ObjectOutputStream(fileout);
+	    out.writeObject(h);
+	    out.close();
+	} catch (Exception e) {
+	    System.out.println(e);
 	}
-}	
-
-
-
+    }
+}
