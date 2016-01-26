672a02bac6175cb176c42154c46397923060fee0
boiseamit <ajain@boisestate.edu>
2016-01-25 17:46:41 -0700

    Update examples for reflection

--- examples/reflection/SelfReflect.java
+++ examples/reflection/SelfReflect.java
@@ -2,23 +2,23 @@ import java.awt.Color;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 
-
 /**
  * The Color class reflects on itself.
+ * 
  * @author amit
  *
  */
-public class SelfReflect {
-	public static void main(String [] args) 
-	{
-		Class<Color> type = Color.class;
-		
-		Method [] methods = type.getMethods();
-		for (int i=0; i< methods.length; i++) 
-			System.out.println( methods[i]);
+public class SelfReflect
+{
+    public static void main(String[] args) {
+	Class<Color> type = Color.class;
 
-		Field [] fields = type.getFields();
-		for (int i=0; i< fields.length; i++) 
-			System.out.println( fields[i]);
-	}
+	Method[] methods = type.getMethods();
+	for (int i = 0; i < methods.length; i++)
+	    System.out.println(methods[i]);
+
+	Field[] fields = type.getFields();
+	for (int i = 0; i < fields.length; i++)
+	    System.out.println(fields[i]);
+    }
 }
