3fafa9d7f4f88020c4c6a758afb144afae24b4b5
Amit Jain <amit@onyx.boisestate.edu>
2016-01-27 16:18:22 -0700

    update test script for UDP example

--- examples/sockets/udp/test.sh
+++ examples/sockets/udp/test.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
+server=kohinoor
 cd ..
 
-java udp.UdpClient1 localhost 5005 &
-java udp.UdpClient1 localhost 5005 &
-java udp.UdpClient1 localhost 5005 &
-java udp.UdpClient1 localhost 5005 &
-java udp.UdpClient1 localhost 5005 &
-java udp.UdpClient1 localhost 5005 &
-java udp.UdpClient1 localhost 5005 &
-java udp.UdpClient1 localhost 5005 &
+java udp.UdpClient1 $server 5005 &
+java udp.UdpClient1 $server 5005 &
+java udp.UdpClient1 $server 5005 &
+java udp.UdpClient1 $server 5005 &
+java udp.UdpClient1 $server 5005 &
+java udp.UdpClient1 $server 5005 &
+java udp.UdpClient1 $server 5005 &
+java udp.UdpClient1 $server 5005 &
 
 wait
