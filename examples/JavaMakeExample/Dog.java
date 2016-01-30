9a9348d259573fa1619ceaf763b6822c7e1f3293
amit <ajain@boisestate.edu>
2016-01-29 19:30:57 -0700

    remove unused example folder

--- examples/JavaMakeExample/Dog.java
+++
@@ -1,8 +0,0 @@
-
-public class Dog extends Mammal
-{
-	public Dog()
-	{
-		MooMoo mine = new MooMoo();
-	}
-}
--- examples/JavaMakeExample/Mammal.java
+++
@@ -1,8 +0,0 @@
-
-public class Mammal
-{
-	public static void main (String [] args)
-	{
-		Dog pet = new Dog();
-	}
-}
