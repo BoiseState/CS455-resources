438ed0b7ece8f8479e103842342d9e8172cc0050
boiseamit <ajain@boisestate.edu>
2016-02-24 16:13:11 -0700

    Remove unused package in rmi ex3

--- examples/rmi/ex3-Client-Callback/clientcallback/server/Request.java
+++
@@ -1,7 +0,0 @@
-package clientcallback.server;
-
-// Could hold basic stuff like authentication, time stamps, etc.
-public class Request implements java.io.Serializable
-{
-    private static final long serialVersionUID = -3561266096606341445L;
-}
--- examples/rmi/ex3-Client-Callback/clientcallback/server/WorkListener.java
+++
@@ -1,9 +0,0 @@
-package clientcallback.server;
-
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface WorkListener extends Remote
-{
-    public void workCompleted(WorkRequest request, Object result) throws RemoteException;
-}
