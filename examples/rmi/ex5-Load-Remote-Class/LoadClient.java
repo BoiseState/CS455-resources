cb5b1ff958dde4c76be06966d7a5e3ddd8ab8c00
boiseamit <ajain@boisestate.edu>
2016-02-29 11:07:10 -0700

    Update Example 5.

--- examples/rmi/ex5-Load-Remote-Class/LoadClient.java
+++ examples/rmi/ex5-Load-Remote-Class/LoadClient.java
@@ -1,36 +1,36 @@
 
-
-import java.rmi.RMISecurityManager;
 import java.rmi.server.RMIClassLoader;
 import java.net.*;
 import java.security.*;
 
 public class LoadClient
 {
-    public static void main(String[] args)
-    {
-		if (args.length == 0) {
-			System.err.println("Usage: java LoadClient <remote URL>");
-			System.exit(1);
-		}
-        System.setSecurityManager(new mySecurityManager());
-        try {
-				URL url = new URL(args[0]);
-				Class cl = RMIClassLoader.loadClass(url,"RunAway");
-                System.out.println(cl);
-                Runnable client = (Runnable)cl.newInstance();
-                client.run();
-				System.exit(0);
-        } catch (Exception e) {
-                System.out.println("Exception: " + e.getMessage());
-                e.printStackTrace();
-        }
+    public static void main(String[] args) {
+	if (args.length == 0) {
+	    System.err.println("Usage: java LoadClient <remote URL>");
+	    System.exit(1);
+	}
+	System.setSecurityManager(new mySecurityManager());
+	try {
+	    URL url = new URL(args[0]);
+	    Class cl = RMIClassLoader.loadClass(url, "client.RunAway");
+	    System.out.println(cl);
+	    Runnable client = (Runnable) cl.newInstance();
+	    client.run();
+	    System.exit(0);
+	} catch (Exception e) {
+	    System.out.println("Exception: " + e.getMessage());
+	    e.printStackTrace();
+	}
     }
 }
 
-class mySecurityManager extends RMISecurityManager
+class mySecurityManager extends SecurityManager
 {
-	public void checkConnect(String host, int port, Object context) {}
-	public void checkPermission(Permission perm) {}
-}
+    public void checkConnect(String host, int port, Object context) {
+    }
 
+
+    public void checkPermission(Permission perm) {
+    }
+}
--- examples/rmi/ex5-Load-Remote-Class/client/RunAway.java
+++ examples/rmi/ex5-Load-Remote-Class/client/RunAway.java
@@ -1,4 +1,4 @@
-
+package client;
 
 public class RunAway 
     implements Runnable {
