e1f28ba04069a88bf75cd45433192a93fce32881
boiseamit <ajain@boisestate.edu>
2016-02-29 09:53:11 -0700

    Update scripts for rmi ex3

--- examples/rmi/ex3-Client-Callback/test-multiple-clients.sh
+++ examples/rmi/ex3-Client-Callback/test-multiple-clients.sh
@@ -2,9 +2,15 @@
 # Run the server first and then run this and watch server output to check
 # if RMI is multi-threaded or not.
 
+case $# in
+0) echo "Usage: "`basename $0` " <server hostname>"; exit 1;;
+esac
+serverHost=$1
+
 sleep 2
-java -Djava.security.policy=mysecurity.policy callback.client.MyClient localhost &
-java -Djava.security.policy=mysecurity.policy callback.client.MyClient localhost &
-java -Djava.security.policy=mysecurity.policy callback.client.MyClient localhost &
-java -Djava.security.policy=mysecurity.policy callback.client.MyClient localhost &
-java -Djava.security.policy=mysecurity.policy callback.client.MyClient localhost &
+java -Djava.security.policy=mysecurity.policy callback.client.MyClient $serverHost &
+java -Djava.security.policy=mysecurity.policy callback.client.MyClient $serverHost &
+java -Djava.security.policy=mysecurity.policy callback.client.MyClient $serverHost &
+java -Djava.security.policy=mysecurity.policy callback.client.MyClient $serverHost &
+java -Djava.security.policy=mysecurity.policy callback.client.MyClient $serverHost &
+
