1c18a1177fb87ddbf16f8a479a503a0b80f1dca0
boiseamit <ajain@boisestate.edu>
2016-03-07 15:24:20 -0700

    Update mcast examples

---
+++ examples/multicasting/ex0-mcast-setup/PrintNetworkInterfaces.java
@@ -0,0 +1,24 @@
+
+import java.net.*;
+
+public class PrintNetworkInterfaces
+{
+    public static void main(String args[]) throws SocketException {
+	if (args.length == 0) {
+	    System.err.println("Usage: java PrintNetworkInterfaces <interface>");
+	    System.exit(1);
+	}
+	NetworkInterface net1 = NetworkInterface.getByName(args[0]);
+	System.out.println(net1);
+	System.out.println("Support multicast=" + net1.supportsMulticast());
+
+	/* NetworkInterface net2 = NetworkInterface.getByName("eth1"); */
+	/* System.out.println(net2); */
+	/* System.out.println("Support multicast="+net2.supportsMulticast()); */
+
+	NetworkInterface net3 = NetworkInterface.getByName("lo");
+	System.out.println(net3);
+	System.out.println("Support multicast=" + net3.supportsMulticast());
+    }
+
+}
