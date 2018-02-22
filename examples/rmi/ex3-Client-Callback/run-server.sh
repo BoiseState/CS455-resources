2d9e19d71f6bb2a4c9ef153bc92e3e34f87338a6
Amit Jain <ajain@boisestate.edu>
2018-02-22 10:41:51 -0700

    Update rmi ex3

--- examples/rmi/ex3-Client-Callback/run-server.sh
+++ examples/rmi/ex3-Client-Callback/run-server.sh
@@ -3,8 +3,8 @@
 echo "Killing existing rmiregistry"
 killall -9 rmiregistry >& /dev/null
 
-echo "Starting new rmiregistry from `pwd`"
-export CLASSPATH=`pwd`:$CLASSPATH
+echo "Starting new rmiregistry from $(pwd)"
+export CLASSPATH=$(pwd):$CLASSPATH
 
 rmiregistry &
 
