1e7e8017448fff0f5e9783c961cd84c1853217cf
Amit Jain <ajain@boisestate.edu>
2018-03-01 11:27:49 -0700

    Update formatting

--- examples/rmi/ex7-PassingArgsInRMI/Doodle.java
+++ examples/rmi/ex7-PassingArgsInRMI/Doodle.java
@@ -2,6 +2,7 @@ import java.io.Serializable;
 
 public class Doodle implements Serializable
 {
+    private static final long serialVersionUID = 8850092555201204638L;
     private String dood = "Googoo-gaga";
 
 
