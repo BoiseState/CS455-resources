0ea443fdce958f60f9e8bd75c2f78682b4588e59
Amit Jain <ajain@boisestate.edu>
2018-04-24 15:09:28 -0600

    add lambda examples

---
+++ examples/lambdas/ThreadLambdaEx1.java
@@ -0,0 +1,19 @@
+
+
+public class ThreadLambdaEx1 {
+  public static void main(String[] args) {
+    Runnable r1 = new Runnable() {
+      @Override
+      public void run() {
+        System.out.println("No lambda");
+      }
+    };
+
+    Runnable r2 = () -> { System.out.println("With Lambda"); };
+
+    new Thread(r1).start();
+    new Thread(r2).start();
+    
+    new Thread(() -> System.out.println("With Lambda as an argument!")).start();;
+  }
+}
