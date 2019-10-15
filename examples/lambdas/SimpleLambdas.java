a3b9f31a74ed58714652d84e5ae725afd9f52be9
Amit Jain <ajain@boisestate.edu>
2019-10-14 19:13:28 -0600

    Add new lambda example

---
+++ examples/lambdas/SimpleLambdas.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+
+public class SimpleLambdas
+{
+    public static void main(String args[]) {
+	// Creating an ArrayList with elements
+	// {1, 2, 3, 4}
+	ArrayList<Integer> l = new ArrayList<Integer>();
+	l.add(1);
+	l.add(2);
+	l.add(3);
+	l.add(4);
+
+	// Using lambda expression to print all elements of l
+	l.forEach(n -> System.out.println(n));
+	System.out.println();
+
+	// Using lambda expression to print even elements of l
+	l.forEach(n -> {
+	    if (n % 2 == 0) System.out.println(n);
+	});
+    }
+}
