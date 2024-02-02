821816904e1a68e17e98874f6f1ef1af39a062a4
Amit Jain <ajain@boisestate.edu>
2024-02-02 11:06:21 -0700

    Add threadpool with Lambda example

--- examples/sockets/tcp/objectserver/MyCalculation.java
+++ examples/sockets/tcp/objectserver/MyCalculation.java
@@ -5,14 +5,16 @@ public class MyCalculation extends WorkRequest
     private static final long serialVersionUID = -782657330809139571L;
     private int[] values;
 
-    public MyCalculation(int n) {
-        values = new int[n];
+    public MyCalculation(int n)
+    {
+	values = new int[n];
     }
 
 
-    public Object execute() {
-        for (int i = 0; i < values.length; i++)
-            values[i] = i * i;
-        return values;
+    public Object execute()
+    {
+	for (int i = 0; i < values.length; i++)
+	    values[i] = i * i;
+	return values;
     }
 }
