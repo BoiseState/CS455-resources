cf7165739385ecfaa3fa0e5f02c46521e01fa0c1
amit <ajain@boisestate.edu>
2024-04-09 23:42:03 -0600

    Update hw4

---
+++ projects/hw4/TimeClient.java
@@ -0,0 +1,36 @@
+package tcp.singlethreaded;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.ObjectInputStream;
+import java.net.Socket;
+import java.util.Date;
+
+/**
+ * Connect to a time server and get time from it as a Date object.
+ */
+public class TimeClient
+{
+    public static void main(String args[]) {
+        if (args.length != 2) {
+            System.err.println("Usage: java TimeClient <serverhost> <port>");
+            System.exit(1);
+        }
+        String host = args[0];
+        int port = Integer.parseInt(args[1]);
+
+        try {
+            Socket s = new Socket(host, port);
+            InputStream in = s.getInputStream();
+            ObjectInputStream objin = new ObjectInputStream(in);
+
+            Date date = (Date) objin.readObject();
+            System.out.println("Time on host " + host + " is " + date);
+            s.close();
+        } catch (IOException e1) {
+            System.out.println(e1);
+        } catch (ClassNotFoundException e2) {
+            System.out.println(e2);
+        }
+    }
+}
---
+++ projects/hw4/TimeServer.java
@@ -0,0 +1,69 @@
+package tcp.singlethreaded;
+
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.InetAddress;
+
+/**
+ * A single-threaded time server that sends Date objects to clients. You may
+ * need to open port 5005 in the firewall on the host machine (unless you are
+ * locally).
+ * 
+ * @author amit
+ */
+
+public class TimeServer
+{
+    private OutputStream out;
+    private int port = 5005;
+    private ServerSocket s;
+
+    public static void main(String args[]) {
+        TimeServer server = new TimeServer();
+        server.serviceClients();
+    }
+
+
+    public TimeServer() {
+        try {
+            s = new ServerSocket(port);
+            System.out.println("TimeServer: up and running on port " + port + " " + InetAddress.getLocalHost());
+        } catch (IOException e) {
+            System.err.println(e);
+        }
+    }
+
+
+    /**
+     * The method that handles the clients, one at a time.
+     */
+    public void serviceClients() {
+        Socket sock;
+
+        while (true) {
+            try {
+                sock = s.accept();
+                out = sock.getOutputStream();
+
+                // Note that client gets a temporary/transient port on it's side
+                // to talk to the server on its well known port
+                System.out.println("TimeServer: Received connect from " + sock.getInetAddress().getHostAddress() + ": "
+                        + sock.getPort());
+
+                ObjectOutputStream objout = new ObjectOutputStream(out);
+                objout.writeObject(new java.util.Date());
+                objout.flush();
+
+                Thread.sleep(4000); //4 secs
+                sock.close();
+            } catch (InterruptedException e) {
+                System.err.println(e);
+            } catch (IOException e) {
+                System.err.println(e);
+            }
+        }
+    }
+}
