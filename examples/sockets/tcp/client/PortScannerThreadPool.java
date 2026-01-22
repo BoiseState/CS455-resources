53308c62aed9a645d041ad8b9c5a23e335236f9c
amit <ajain@boisestate.edu>
2026-01-22 13:46:18 -0700

    Update examples

--- examples/sockets/tcp/client/PortScannerThreadPool.java
+++ examples/sockets/tcp/client/PortScannerThreadPool.java
@@ -1,3 +1,5 @@
+package tcp.client;
+
 import java.io.IOException;
 import java.net.Socket;
 import java.util.concurrent.Executor;
--- examples/sockets/tcp/client/PortScannerThreadPoolLambda.java
+++ examples/sockets/tcp/client/PortScannerThreadPoolLambda.java
@@ -1,6 +1,7 @@
+package tcp.client;
+
 import java.io.IOException;
 import java.net.Socket;
-import java.util.concurrent.Executor;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
