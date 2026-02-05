8949e67ab5222d4e9c77b395f5e518f04c472029
amit <ajain@boisestate.edu>
2026-02-05 14:07:25 -0700

    Update examples

--- examples/sockets/largerhttpd/ClientQueue.java
+++ examples/sockets/largerhttpd/ClientQueue.java
@@ -5,19 +5,21 @@ import java.util.ArrayList;
 
 public class ClientQueue extends ArrayList<SelectionKey>
 {
-    public synchronized boolean add(SelectionKey key) {
-	super.add(key);
-	notify();
-	return true;
-    }
+	public synchronized boolean add(SelectionKey key)
+	{
+		super.add(key);
+		notify();
+		return true;
+	}
 
-
-    public synchronized SelectionKey next() {
-	while (isEmpty())
-	    try {
-		wait();
-	    } catch (InterruptedException e) {
-	    }
-	return remove(0);
-    }
+	public synchronized SelectionKey next()
+	{
+		while (isEmpty())
+			try {
+				wait();
+			} catch (InterruptedException e) {
+				System.err.println(e);
+			}
+		return remove(0);
+	}
 }
